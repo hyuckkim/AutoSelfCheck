1bffc7b55e6e8e406859a58f41532102b8edc786
hyuckkim <53367179+hyuckkim@users.noreply.github.com>
2020-06-26 15:46:59 +0900

    프로그램 모듈화(조금)
    또 여러명 입력할 수 있게됨.

--- AutoSelfCheck.py
+++ AutoSelfCheck.py
@@ -1,44 +1,78 @@
 # coding=utf-8
+import os.path
 from selenium import webdriver
+import collections
 
-chromedriver = '/home/hyuck/chromedriver' # 리눅스
+StudentInfo = collections.namedtuple(
+    'StudentInfo', 
+    [
+        'schoolName',
+        'myName',
+        'citizenNum'
+    ]
+)
+MY_PATH = os.path.abspath(os.path.dirname(__file__))
+
+
+def SearchSite(school, my, citizen):
+    # url에 접근한다.
+    if first != True:
+        driver.execute_script('window.open("about:blank", "_blank");')
+        driver.switch_to_window(driver.window_handles[len(driver.window_headles) - 1])
+    driver.get(site)
+
+    #3초쯤 그냥 기다려 주자.
+    driver.implicitly_wait(3)
+
+    driver.find_element_by_xpath('//*[@title="학생정보 입력"]/div').click()
+
+    #3초쯤 그냥 기다려 주자.
+    driver.implicitly_wait(3)
+
+    driver.find_element_by_xpath('//*[@id="btnSrchSchul"]').click()
+
+    while len(driver.window_handles) == 1:
+        driver.implicitly_wait(1)
+    driver.switch_to_window(driver.window_handles[len(driver.window_headles) - 1])
+    driver.find_element_by_xpath('//*[@id="schulNm"]').send_keys(school)
+    driver.find_element_by_xpath('//*[@id="infoForm"]/div[1]/p/span[3]/button').click()
+    driver.find_element_by_xpath('//*[@id="infoForm"]/div[2]/table/tbody/tr/td[1]/a').click()
+
+    driver.switch_to_window(driver.window_handles[len(driver.window_headles) - 1])
+    driver.find_element_by_name('pName').send_keys(' ' + my)
+    driver.find_element_by_name('frnoRidno').send_keys(citizen)
+    driver.find_element_by_xpath('//*[@id="btnConfirm"]').click()
+
+    #3초쯤 그냥 기다려 주자.
+    driver.implicitly_wait(3)
+
+    driver.find_element_by_xpath('//*[@id="infoForm"]/div/div/table[1]/tbody/tr[2]/td/span/label[1]').click()
+    driver.find_element_by_xpath('//*[@id="infoForm"]/div/div/table[2]/tbody/tr[2]/td/span/label[1]').click()
+    driver.find_element_by_xpath('//*[@id="infoForm"]/div/div/table[3]/tbody/tr[2]/td/span/label[1]').click()
+    driver.find_element_by_xpath('//*[@id="infoForm"]/div/div/table[4]/tbody/tr[2]/td/span/label[1]').click()
+    driver.find_element_by_xpath('//*[@id="infoForm"]/div/div/table[5]/tbody/tr[2]/td/span/label[1]').click()
+
+
+def RemoveEnter(str):
+    if(str[-1] == '\n'):
+        return str[:-1]
+
+path = os.path.join(MY_PATH, 'config.txt')
+configfile = open(path,'r')
+originalconfigs = [RemoveEnter(x) for x in configfile.readlines()]
+
+site = originalconfigs[0]
+chromedriver = originalconfigs[1]
+Students = []
+for x in originalconfigs[2:]:
+    y = x.split()
+    Students.append(StudentInfo(schoolName = y[0], myName = y[1], citizenNum = y[2]))
+
 driver = webdriver.Chrome(chromedriver)
 
-schoolName = '선인고등학교'
-myName = '홍길동'
-citizenNum = '123456'
+first = True
+for x in Students:
+    SearchSite(x.schoolName, x.myName, x.citizenNum)
+    first = False
 
-# url에 접근한다.
-driver.get('https://eduro.ice.go.kr/hcheck/index.jsp')
-
-#3초쯤 그냥 기다려 주자.
-driver.implicitly_wait(3)
-
-driver.find_element_by_xpath('//*[@title="학생정보 입력"]/div').click()
-
-#3초쯤 그냥 기다려 주자.
-driver.implicitly_wait(3)
-
-driver.find_element_by_xpath('//*[@id="btnSrchSchul"]').click()
-
-while len(driver.window_handles) == 1:
-    driver.implicitly_wait(1)
-print(driver.window_handles)
-driver.switch_to_window(driver.window_handles[1])
-driver.find_element_by_xpath('//*[@id="schulNm"]').send_keys(schoolName)
-driver.find_element_by_xpath('//*[@id="infoForm"]/div[1]/p/span[3]/button').click()
-driver.find_element_by_xpath('//*[@id="infoForm"]/div[2]/table/tbody/tr/td[1]/a').click()
-
-driver.switch_to_window(driver.window_handles[0])
-driver.find_element_by_name('pName').send_keys(' ' + myName)
-driver.find_element_by_name('frnoRidno').send_keys(citizenNum)
-driver.find_element_by_xpath('//*[@id="btnConfirm"]').click()
-
-#3초쯤 그냥 기다려 주자.
-driver.implicitly_wait(3)
-
-driver.find_element_by_xpath('//*[@id="infoForm"]/div/div/table[1]/tbody/tr[2]/td/span/label[1]').click()
-driver.find_element_by_xpath('//*[@id="infoForm"]/div/div/table[2]/tbody/tr[2]/td/span/label[1]').click()
-driver.find_element_by_xpath('//*[@id="infoForm"]/div/div/table[3]/tbody/tr[2]/td/span/label[1]').click()
-driver.find_element_by_xpath('//*[@id="infoForm"]/div/div/table[4]/tbody/tr[2]/td/span/label[1]').click()
-driver.find_element_by_xpath('//*[@id="infoForm"]/div/div/table[5]/tbody/tr[2]/td/span/label[1]').click()
+x = input("작동이 끝났습니다. 끝내려면 Enter 키를 눌러주세요...")
